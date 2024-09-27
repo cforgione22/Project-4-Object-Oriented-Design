0d962d3a8269275128f50fa915ac0f9bbd787952
cforgione22 <crystal.forgione22@gmail.com>
2024-09-27 12:17:19 -0400

    getters and setters

--- src/main/java/org/example/EukaryaDomain.java
+++ src/main/java/org/example/EukaryaDomain.java
@@ -22,4 +22,59 @@ public class EukaryaDomain {
 
     }
 
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getGenusName() {
+        return genusName;
+    }
+
+    public void setGenusName(String genusName) {
+        this.genusName = genusName;
+    }
+
+    public String getSpeciesName() {
+        return speciesName;
+    }
+
+    public void setSpeciesName(String speciesName) {
+        this.speciesName = speciesName;
+    }
+
+    public String getOrganismName() {
+        return organismName;
+    }
+
+    public void setOrganismName(String organismName) {
+        this.organismName = organismName;
+    }
+
+    public boolean isExtinct() {
+        return extinct;
+    }
+
+    public void setExtinct(boolean extinct) {
+        this.extinct = extinct;
+    }
 }
